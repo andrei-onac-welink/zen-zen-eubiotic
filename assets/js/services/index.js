98d71db78054c594088946d4e6034a1abc7c6b9c
Andrei Onac <andrei.onac@welink.ro>
2026-08-13 15:42:43 +0300

    feat: Add Zentiva Cookie Consent

--- assets/js/services/index.js
+++ assets/js/services/index.js
@@ -1,7 +1,6 @@
 import './aos.service';
 import './backToTop.service';
 import './blogScrollspy.service';
-import './cookieConsent.service';
 import './counter.service';
 import './header.service';
 import './swiper.service';
